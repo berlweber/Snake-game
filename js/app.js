d8aaf30ab0a192be3fbfa9905ff0e6a76bbddcb0
berl weber <153825363+berlweber@users.noreply.github.com>
2025-12-15 12:06:02 +0100

    cached the message element

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@
 /*----- Cached Element References  -----*/
 const startStpBtn = document.querySelector('#strt-stp');
 const resetBtn = document.querySelector('#reset');
-console.log(resetBtn);
+const messageEl = document.querySelector('#message');
+console.log(messageEl);
 
 /*-------------- Functions -------------*/
 
