3948a67103198779a8c03c7680f97cfa02c354be
berl weber <153825363+berlweber@users.noreply.github.com>
2025-12-15 22:17:45 +0100

    1. created setGameField function that sets all values for the game field grid and stores them in the gameField object, 2. created renderGameField function. 3. created render function and called renderGameField init, 4. created init function, 5. created eventlistener and function adjustToWindow

--- js/app.js
+++ js/app.js
@@ -21,18 +21,38 @@ const setGameField = () => {
     gameField.width = gameFieldEl.clientWidth;
     gameField.hight = gameFieldEl.clientHeight;
     gameField.columnAndRowWidth = 10;
-    gameField.ColumnsAmount = Math.floor(gameField.width / gameField.columnAndRowWidth);
+    gameField.adjustedWidth = gameField.width - (gameField.width % gameField.columnAndRowWidth);
+    gameField.adjustedHeight = gameField.hight - (gameField.hight % gameField.columnAndRowWidth);
+    gameField.columnsAmount = Math.floor(gameField.width / gameField.columnAndRowWidth);
     gameField.rowsamount = Math.floor(gameField.hight / gameField.columnAndRowWidth);
 }
-setGameField();
 
+const renderGameField = () => {
+    gameFieldEl.style.gridTemplateColumns = `repeat(${gameField.columnsAmount}, ${gameField.columnAndRowWidth}px)`;
+    gameFieldEl.style.gridTemplateRows = `repeat(${gameField.rowsamount}, ${gameField.columnAndRowWidth}px)`;
+    //gameFieldEl.style.width = `${gameField.adjustedWidth}`; need to update width to round grid tightly
+    //gameFieldEl.style.hight = `${gameField.adjustedWidth}`; need to update width to round grid tightly
+}
 
 const createMsg = () => {
 
 }
 
-const init = () => {
+const render = () => {
+    renderGameField();
+};
 
-}
+const init = () => {
+    setGameField();
+    render();
+};
+
+init();
+
+const adjustToWindow = () =>{
+    setGameField();
+    renderGameField();
+};
 /*----------- Event Listeners ----------*/
 
+window.addEventListener("resize", adjustToWindow);
