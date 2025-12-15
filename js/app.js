5240728e00b3975ef9db4c29928aa9afa109bd65
berl weber <153825363+berlweber@users.noreply.github.com>
2025-12-15 19:46:48 +0100

    created the gameField object and setGameField function to set it.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*-------------- Constants -------------*/
+//msg cost here
 const highScore = [];//for level-up
-
+const gameField = {};
 /*---------- Variables (state) ---------*/
 let playing;
 let score;
@@ -12,10 +13,26 @@ const resetBtn = document.querySelector('#reset');
 const gameFieldEl = document.querySelector('#game-field');
 const snakeEl = document.querySelector('#snake');
 const messageEl = document.querySelector('#message');
+console.log(gameFieldEl);
 console.log(snakeEl);
 
 /*-------------- Functions -------------*/
+const setGameField = () => {
+    gameField.width = gameFieldEl.clientWidth;
+    gameField.hight = gameFieldEl.clientHeight;
+    gameField.columnAndRowWidth = 10;
+    gameField.ColumnsAmount = Math.floor(gameField.width / gameField.columnAndRowWidth);
+    gameField.rowsamount = Math.floor(gameField.hight / gameField.columnAndRowWidth);
+}
+setGameField();
 
 
+const createMsg = () => {
+
+}
+
+const init = () => {
+
+}
 /*----------- Event Listeners ----------*/
 
