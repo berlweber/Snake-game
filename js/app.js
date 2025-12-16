b549a58aacbeade003b03a150d4af5b23e1a5b3a
berl weber <153825363+berlweber@users.noreply.github.com>
2025-12-16 16:22:32 +0100

    changed from css grid to a grid thourgh a flex-box filled with div-cells with js, deleted snake and food divs and renderGameField function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 //msg cost here
 const highScore = [];//for level-up
 const gameField = {};
+
 /*---------- Variables (state) ---------*/
 let playing;
 let score;
@@ -11,35 +12,53 @@ let timerId;
 const startStpBtn = document.querySelector('#strt-stp');
 const resetBtn = document.querySelector('#reset');
 const gameFieldEl = document.querySelector('#game-field');
+const gameFieldCells = gameFieldEl.children;
 const snakeEl = document.querySelector('#snake');
 const messageEl = document.querySelector('#message');
 console.log(gameFieldEl);
-console.log(snakeEl);
 
 /*-------------- Functions -------------*/
+/*const createGrid () => {
+	
+}*/
 const setGameField = () => {
     gameField.width = gameFieldEl.clientWidth;
     gameField.hight = gameFieldEl.clientHeight;
     gameField.columnAndRowWidth = 10;
     gameField.adjustedWidth = gameField.width - (gameField.width % gameField.columnAndRowWidth);
     gameField.adjustedHeight = gameField.hight - (gameField.hight % gameField.columnAndRowWidth);
-    gameField.columnsAmount = Math.floor(gameField.width / gameField.columnAndRowWidth);
-    gameField.rowsamount = Math.floor(gameField.hight / gameField.columnAndRowWidth);
+    gameField.columnsAmount = Math.floor(gameField.width / gameField.columnAndRowWidth);//to be removed
+    gameField.rowsAmount = Math.floor(gameField.hight / gameField.columnAndRowWidth);
+		gameField.cellsAmount = gameField.columnsAmount * gameField.rowsAmount;
+		gameField.cells = [];
+		if (gameFieldCells.length > 0){
+			gameFieldCells.array.forEach(element => {
+				element.remove();
+			});
+		}
+		for (let i = 0; i < gameField.cellsAmount; i++){
+			const cell = document.createElement('div');
+			gameField.cells.push(cell);
+			gameFieldEl.appendChild(cell);
+		};
+		//console.log(gameFieldCells);
 }
-
-const renderGameField = () => {
+//console.log(gameField.hight);
+/*const renderGameField = () => {
     gameFieldEl.style.gridTemplateColumns = `repeat(${gameField.columnsAmount}, ${gameField.columnAndRowWidth}px)`;
-    gameFieldEl.style.gridTemplateRows = `repeat(${gameField.rowsamount}, ${gameField.columnAndRowWidth}px)`;
+    gameFieldEl.style.gridTemplateRows = `repeat(${gameField.rowsAmount}, ${gameField.columnAndRowWidth}px)`;
     //gameFieldEl.style.width = `${gameField.adjustedWidth}`; need to update width to round grid tightly
     //gameFieldEl.style.hight = `${gameField.adjustedWidth}`; need to update width to round grid tightly
-}
+}*/
+
+
 
 const createMsg = () => {
 
 }
 
 const render = () => {
-    renderGameField();
+    
 };
 
 const init = () => {
@@ -51,7 +70,6 @@ init();
 
 const adjustToWindow = () =>{
     setGameField();
-    renderGameField();
 };
 /*----------- Event Listeners ----------*/
 
